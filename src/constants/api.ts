7fe0e280670e5b69785a9e5b50bdb8bb015cb83c
Newname <new@email>
2007-05-19 01:01:01 -0700

    [*] api update

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,6 +1,6 @@
 export const BASE_URL_API: string = 'https://erdsea.com';
-export const ELROND_API: string = 'https://devnet-api.elrond.com';
-export const ELROND_GATEWAY_API: string = 'https://devnet-gateway.elrond.com';
+export const ELROND_API: string = 'https://api.elrond.com';
+export const ELROND_GATEWAY_API: string = 'https://gateway.elrond.com';
 
 export const GET = 'GET';
 export const PUT: string = 'PUT';
