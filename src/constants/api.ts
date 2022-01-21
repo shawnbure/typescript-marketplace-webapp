7ccd859b678a743eeb4ea9fe0862d348f3a09596
Shawn Bure <shawnbure@outlook.com>
2022-01-21 12:11:36 -0700

    api change

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
-export const BASE_URL_API: string = 'https://test-api.youbei.io';
+export const BASE_URL_API: string = 'https://container-service-youbei-api-test.3dlo0nfncitse.us-east-2.cs.amazonlightsail.com';
 export const ELROND_API: string = 'https://api.elrond.com';
 export const ELROND_GATEWAY_API: string = 'https://gateway.elrond.com';
 //dev comment
@@ -6,4 +6,3 @@ export const GET = 'GET';
 export const PUT: string = 'PUT';
 export const POST = 'POST';
 export const PATCH = 'PATCH';
-
