6ad674d4c007115fb27f961d817bb6a526dad92a
Shawn Bure <shawnbure@outlook.com>
2022-01-21 14:31:45 -0700

    api url

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,5 @@
 export const BASE_URL_API: string = 'https://test-api.youbei.io';
+//export const BASE_URL_API: string = 'http://localhost:5000';
 export const ELROND_API: string = 'https://api.elrond.com';
 export const ELROND_GATEWAY_API: string = 'https://gateway.elrond.com';
 export const GET = 'GET';
