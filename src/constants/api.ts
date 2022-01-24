58cb9cb66309a31e286791c6317e966b1868c99a
Shawn Bure <shawnbure@outlook.com>
2022-01-24 00:19:58 -0700

    test new service

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
-export const BASE_URL_API: string = 'https://test-api.youbei.io:5055';
+export const BASE_URL_API: string = 'https://youbei-api-test.3dlo0nfncitse.us-east-2.cs.amazonlightsail.com:5000';
 //export const BASE_URL_API: string = 'http://localhost:5000';
 export const ELROND_API: string = 'https://api.elrond.com';
 export const ELROND_GATEWAY_API: string = 'https://gateway.elrond.com';
