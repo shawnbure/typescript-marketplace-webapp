7bb7c257a0df5b84798c329e74e9cff4f6bf26dd
Shawn Bure <shawnbure@outlook.com>
2022-01-23 23:15:21 -0700

    test set up

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
-export const BASE_URL_API: string = 'https://test-api.youbei.io:5005';
+export const BASE_URL_API: string = 'https://test-api.youbei.io:5055';
 //export const BASE_URL_API: string = 'http://localhost:5000';
 export const ELROND_API: string = 'https://api.elrond.com';
 export const ELROND_GATEWAY_API: string = 'https://gateway.elrond.com';
