ea71fa9644d8ae3538493142d3be77c9b8b5ab42
Shawn Bure <shawnbure@outlook.com>
2022-01-30 15:10:32 -0700

    Changed constants/api.ts with dev url ]

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,6 +1,6 @@
 export const BASE_URL_API: string = process.env.NODE_ENV == 'development' ? 'https://dev-api.youbei.io' : process.env.NODE_ENV == 'production' ? 'https://api.youbei.io' : 'http://localhost:5000';
-export const ELROND_API: string = process.env.NODE_ENV == 'development' ? 'https://testnet-api.elrond.com' : process.env.NODE_ENV == 'production' ? 'https://api.elrond.com' : 'https://devnet-api.elrond.com'; 
-export const ELROND_GATEWAY_API: string =  process.env.NODE_ENV == 'development' ? 'https://testnet-gateway.elrond.com' : process.env.NODE_ENV == 'production' ? 'https://gateway.elrond.com' : 'https://devnet-gateway.elrond.com'; 
+export const ELROND_API: string = process.env.NODE_ENV == 'development' ? 'https://devnet-api.elrond.com' : process.env.NODE_ENV == 'production' ? 'https://api.elrond.com' : 'https://testnet-api.elrond.com'; 
+export const ELROND_GATEWAY_API: string =  process.env.NODE_ENV == 'development' ? 'https://devnet-gateway.elrond.com' : process.env.NODE_ENV == 'production' ? 'https://gateway.elrond.com' : 'https://testnet-gateway.elrond.com'; 
 
 export const GET = 'GET';
 export const PUT: string = 'PUT';
