fe591d37ead9698d5f24282a3d929f1413c905f6
Amir Nouri <anasvsfs@gmail.com>
2022-02-24 23:32:19 +0330

    token image

--- src/containers/pages/CollectionPage/index.tsx
+++ src/containers/pages/CollectionPage/index.tsx
@@ -824,7 +824,7 @@ export const CollectionPage: (props: any) => any = ({ }) => {
                                                         <div className={`c-card c-card--colection`}>
 
                                                             <div className="c-card_img-container">
-                                                                <img src={formatHexMetaImage(token.imageLink)} className="c-card_img" alt="" />
+                                                                <img src={(token.imageLink)} className="c-card_img" alt="" />
                                                             </div>
 
                                                             <div className="c-card_info">
