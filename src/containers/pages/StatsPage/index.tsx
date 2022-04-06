1337362af14f2d1ed6071f814b3ff5b813745211
Anthony <anthony@elrondnft.io>
2022-04-06 17:38:15 +0430

    Create Main Structure Of Page

--- src/containers/pages/StatsPage/index.tsx
+++ src/containers/pages/StatsPage/index.tsx
@@ -1,7 +1,12 @@
 export const StatsPage = () => {
     return (
-        <div className="conatiner">
-            Stats Page
+        <div className="stats-conatiner">
+            <div className="stats-reportBox">
+                <div className="stats-numericResults">Reports</div>
+                <div className="stats-volumeChart">Chart</div>
+                <div className="stats-topCollections">Top Collections</div>
+            </div>
+            <div className="stats-activitiesBox">Activities</div>
         </div>
     );
 };
