b34a234edf213d19a6dd17a71249a28b21bea4c2
“Shawn <shawnbure@outlook.com>
2022-04-09 13:11:12 -0700

    confirmation min and fail

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,8 +1,11 @@
 //#############################################################Confirmation Page#############################################################//
+
 export const ENG_BUY_TITLE: string = "Congratulations!";
 export const ENG_BUY_MESSAGE: string = "You have purchased {{tokenName}}!";
 export const ENG_LIST_TITLE: string = "Success!";
-export const ENG_LIST_MESSAGE: string = "You have listed {{tokenName}} for  {{priceNominal}} eGLD on the Youbei Marketplace.";
+export const ENG_LIST_MESSAGE: string = "You have listed {{tokenName}} for {{priceNominal}} eGLD on the Youbei Marketplace.";
+export const ENG_MINT_TITLE: string = "Success!";
+export const ENG_MINT_MESSAGE: string = "You have minted {{numMint}} {{collectionName}} tokens on the Youbei Marketplace.";
 export const ENG_WITHDRAW_TITLE: string = "Success!";
 export const ENG_WITHDRAW_MESSAGE: string = "You have withdrawn {{tokenName}} from the Youbei Marketplace.";
 export const ENG_START_AUCTION_TITLE: string = "Congratulations!";
@@ -17,4 +20,9 @@ export const ENG_DEFAULT_CONFIRMATION_TITLE: string = "Default Success";
 export const ENG_DEFAULT_CONFIRMATION_MESSAGE: string = "default";
 export const ENG_TX_PROCESSING_MESSAGE: string = "Your transaction has been recorded on the Youbei Marketplace and sent to the Elrond network. Currently, we are verifying the status and some features will be available after your transaction is complete.";
 export const ENG_TX_SUCCESS_MESSAGE: string = "The network has successfully finished your transaction for {{tokenName}}."
+export const ENG_TX_COMPETED_MESSAGE: string = "Your transaction has completed.";
+export const ENG_TX_FAILED_MESSAGE: string = "Your transaction has failed. Please try the transaction again. Transaction Hash: {{txHash}}";
+export const ENG_COLLECTION_BUTTON = "See Collection";
+export const ENG_LOADING_ASSET = "loading asset...";
+
 //#############################################################Confirmation Page#############################################################//
