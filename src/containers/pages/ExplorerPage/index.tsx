186cc78055414149ff74cb02e61e715ed300dd47
Anthony <anthony@elrondnft.io>
2022-05-06 09:47:22 +0430

    Complete Structure

--- src/containers/pages/ExplorerPage/index.tsx
+++ src/containers/pages/ExplorerPage/index.tsx
@@ -15,12 +15,72 @@ export const ExplorerPage = () => {
                     <div className="explorer-filterBox__filters">
                         <div>
                             <button>
-                                <FontAwesomeIcon style={{margin: '0 8px 0 0'}} icon={faIcons.faShoppingBag} /> Sale
-                                type
+                                <FontAwesomeIcon
+                                    style={{
+                                        margin: "0 8px 0 0",
+                                        opacity: "0.5",
+                                    }}
+                                    icon={faIcons.faShoppingBag}
+                                />{" "}
+                                Sale type
                             </button>
-                            <span style={{background: '#E2204E'}}>3</span>
+                        </div>
+
+                        <div>
+                            <button>
+                                <FontAwesomeIcon
+                                    style={{
+                                        margin: "0 8px 0 0",
+                                        opacity: "0.5",
+                                    }}
+                                    icon={faIcons.faWindowMaximize}
+                                />{" "}
+                                Collections
+                            </button>
+                            <span style={{ background: "#E2204E" }}>1</span>
+                        </div>
+
+                        <div>
+                            <button>
+                                <FontAwesomeIcon
+                                    style={{
+                                        margin: "0 8px 0 0",
+                                        opacity: "0.5",
+                                    }}
+                                    icon={faIcons.faFilter}
+                                />{" "}
+                                Options
+                            </button>
+                        </div>
+
+                        <div>
+                            <button>
+                                <FontAwesomeIcon
+                                    style={{
+                                        margin: "0 8px 0 0",
+                                        opacity: "0.5",
+                                    }}
+                                    icon={faIcons.faLayerGroup}
+                                />{" "}
+                                Sort
+                            </button>
+                            <span style={{ background: "#2081E2" }}>2</span>
+                        </div>
+
+                        <div>
+                            <button>
+                                <FontAwesomeIcon
+                                    style={{
+                                        margin: "0 8px 0 0",
+                                        opacity: "0.5",
+                                    }}
+                                    icon={faIcons.faMoneyBill}
+                                />{" "}
+                                Price range
+                            </button>
                         </div>
                     </div>
+
                     <div className="explorer-filterBox__info">
                         <div>
                             <span>524</span>
@@ -28,11 +88,32 @@ export const ExplorerPage = () => {
                             <span>for exploring</span>
                         </div>
                         <div>
-                            <FontAwesomeIcon icon={faIcons.faAd} />
+                            <FontAwesomeIcon icon={faIcons.faChevronRight} />
                         </div>
                     </div>
                 </div>
-                <div className="explorer-resultBox"></div>
+                <div className="explorer-contentBox">
+                    <div className="explorer-contentBox__content" style={{height: '240px'}}>
+                        <div className="explorer-contentBox__content--image" style={{background: 'url()'}}>
+                            <div>
+                                <span>
+                                    0.02<span> EGLD</span>
+                                </span>
+                            </div>
+                        </div>
+                        <div className="explorer-contentBox__content--details">
+                            <img src="#" />
+                            <div>
+                                <span>Performan #2716</span>
+                                <span>Performan Collection</span>
+                            </div>
+                            <div>
+                                <span>BUY</span>
+                                <span>NOW</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
